d6ac68e34289eb4fb63c5c1fa7c3fcaaadebc526
Ahamed Musthafa <amrs.tech@gmail.com>
2019-07-01 14:26:23 +0530

    Add files via upload

--- ubp/settings.py
+++ ubp/settings.py
@@ -119,5 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
